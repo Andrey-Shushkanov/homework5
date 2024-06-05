1ff3ad88e8fd87b1f53990cd54bf7920b750c5ac
Andrey-Shushkanov <shushkanovandrey@mail.ru>
2024-06-05 22:18:59 +0500

    homework5.py

--- homework5.py
+++ homework5.py
@@ -1,7 +1,7 @@
 immutable_var = True, 12.2, 'Str', 5
-print(immutable_var)
+print('Immutable tuple:', immutable_var)
 # immutable_var[0] = False  Ошибка: Тип переменной 'tuple' не поддерживает изменение
 mutable_list = ['var', 24, 98, False, 'line']
 mutable_list[1] = 'change'
 mutable_list[3] = 12
-print(mutable_list)
+print('Mutable list:', mutable_list)
